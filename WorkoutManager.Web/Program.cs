a0fefd77cb10d21789a6995afe50e27aab859d49
elofubu <elofubu@gmail.com>
2025-10-08 19:58:45 +0200

    cleanup solution and projects

--- WorkoutManager.Web/Program.cs
+++ WorkoutManager.Web/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using _10x_workout_manager;
+using WorkoutManager.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
