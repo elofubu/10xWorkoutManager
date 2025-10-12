69bf24e3ad3e1ef45f996d52f96faf07741bb063
elofubu <elofubu@gmail.com>
2025-10-12 11:57:46 +0200

    rollback first implementation step

--- WorkoutManager.BusinessLogic/Commands/WorkoutPlanCommands.cs
+++ WorkoutManager.BusinessLogic/Commands/WorkoutPlanCommands.cs
@@ -1,25 +1,9 @@
 using MediatR;
 using WorkoutManager.BusinessLogic.DTOs;
-using WorkoutManager.BusinessLogic.Services.Interfaces;
 
 namespace WorkoutManager.BusinessLogic.Commands;
 
 public record GetWorkoutPlanDetailQuery(int WorkoutPlanId, Guid UserId) : IRequest<WorkoutPlanDetailDto>;
-
-public class GetWorkoutPlanDetailQueryHandler : IRequestHandler<GetWorkoutPlanDetailQuery, WorkoutPlanDetailDto>
-{
-    private readonly IWorkoutPlanService _workoutPlanService;
-
-    public GetWorkoutPlanDetailQueryHandler(IWorkoutPlanService workoutPlanService)
-    {
-        _workoutPlanService = workoutPlanService;
-    }
-
-    public async Task<WorkoutPlanDetailDto> Handle(GetWorkoutPlanDetailQuery request, CancellationToken cancellationToken)
-    {
-        return await _workoutPlanService.GetWorkoutPlanAsync(request.WorkoutPlanId, request.UserId, cancellationToken);
-    }
-}
 
 public record CreateWorkoutPlanCommand(string Name, IEnumerable<CreateTrainingDayCommand> TrainingDays);
 
