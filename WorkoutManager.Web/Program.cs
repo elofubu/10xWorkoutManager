b9fffe331f94378d4333fd6c55091c436e9c10a8
elofubu <elofubu@gmail.com>
2025-10-12 13:24:53 +0200

    MudBlazor migration

--- WorkoutManager.Web/Program.cs
+++ WorkoutManager.Web/Program.cs
@@ -8,6 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddBlazorBootstrap();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
